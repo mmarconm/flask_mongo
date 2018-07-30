9b8599302d44e009c78eeab501ee0b2c6c7c5979
Marcelo Marcon <marconm.inf0@gmail.com>
2018-07-29 23:52:09 -0400

    playing with flask and mongodb

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
+from uuid import uuid4
+
+app = Flask(__name__)
+
+client = MongoClient('192.168.0.103')
+db = client.tododb
+
+
+@app.route('/')
+def todo():
+
+    _items = db.tododb.find()
+    count = db.tododb.find().count()
+    items = [item for item in _items]
+
+    return render_template('todo.html', items=items, count=count)
+
+
+@app.route('/new', methods=['POST'])
+def new():
+
+    item_doc = {
+        'id': uuid4().hex,
+        'name': request.form['name'],
+        'description': request.form['description']
+    }
+
+    if item_doc['name'] and item_doc['description']:
+        db.tododb.insert_one(item_doc)
+    else:
+        return redirect(url_for('todo'))
+
+    return redirect(url_for('todo'))
+
+@app.route('/delete/<string:id>', methods=['POST', 'GET'])
+def delete(id):
+    if id:
+        item_doc = {
+            "id": id
+        }
+        db.tododb.remove(item_doc)
+        return redirect(url_for('todo'))
+
+    return 'Wrong'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
